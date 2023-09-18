3a12638a4f7efe116473ee412f29f94298f8245e
luis.nunes <luis.nunes@mail.uft.edu.br>
2023-09-18 11:52:28 -0300

    pagina inicial

--- codeJunior/desafios/urls.py
+++ codeJunior/desafios/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SubmissaoCreateView, DesafiosResolvidosListView
+from .views import SubmissaoCreateView, DesafiosResolvidosListView, DesafiosListView
 from cadastro.views import LoginView, SairView, CadastroView
 
 urlpatterns = [
+    path('', DesafiosListView.as_view(), name="desafios-list"),
     path('tentativa/', SubmissaoCreateView.as_view(), name='submeterTentativa'),
     path('resultado/', DesafiosResolvidosListView.as_view(), name='desafioResolvido'),
 
@@ -10,6 +11,4 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('sair/', SairView.as_view(), name='sair'),
     path('cadastro/', CadastroView.as_view(), name='cadastro'),
-
-
 ]
