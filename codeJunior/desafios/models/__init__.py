9305b08c7695b5ff201c40106042ec63da6119da
Anderson Freitas <freitas.dev@proton.me>
2023-10-08 14:06:38 -0300

    :art: Update Models
    Update Models

--- codeJunior/desafios/models/__init__.py
+++ codeJunior/desafios/models/__init__.py
@@ -1,8 +1,8 @@
 from desafios.models.Desafio import Desafio
 from desafios.models.Submissao import Submissao
 from desafios.models.Trilha import Trilha
+from desafios.models.Linguagem import Linguagem
 
 ### Biblioteca
 
-from desafios.models.biblioteca.repositorio.LinguagemDocumentacao import LinguagemDocumentacao
 from desafios.models.biblioteca.repositorio.Documentacao import Documentacao
