ac1b7840db8604bcd8eea75232e969b332888a86
Anderson Freitas <freitas.dev@proton.me>
2023-10-17 16:10:45 -0300

    Develop (#32)
    * add trilha
    
    * add perfil
    
    * resolvando bug de divisao por zero e alterando estilos
    
    * :zap: Sistema Biblioteca/Repositório de documentação
    
    Sistema Biblioteca/Repositório de documentação
    
    * :art: Update Models
    
    Update Models
    
    * update: models desafios
    
    * :ambulance: Rename model LinguagemDocumentacao to Linguagem
    
    Rename model LinguagemDocumentacao to Linguagem
    
    * adicionando mensagens de erros e fazendo o cadastro funcionar
    
    * adicionando views para create e update de pessoas
    
    * Rename cadastroForm.py to CadastroForm.py
    
    * fazendo as alteracoes pedidas
    
    * removendo a pasta css de assets
    
    * feature/quiz
    
    * Create TutorialDeInstalacao.py
    
    * Add files via upload
    
    * Add files via upload
    
    * :zap: Correção Importação Model Perfil
    
    Correção Importação Model Perfil
    
    * Delete codeJunior/desafios/models/biblioteca/Tutoriais directory
    
    * Create Tutoriais
    
    * Create teste1
    
    * Add files via upload
    
    * :memo: Update requirements
    
    Update requirements
    
    * Feature/forum (#26)
    
    * :art: Initial Forum
    
    Initial Forum
    
    * :art: Estrutura Fórum
    
    Estrutura Fórum
    
    * update/quiz (#27)
    
    * Feature/forum (#29)
    
    * :art: Initial Forum
    
    Initial Forum
    
    * :art: Estrutura Fórum
    
    Estrutura Fórum
    
    * :construction: Work in progress
    
    Work in progress
    
    * :art: Add Estrutura Forum
    
    Add Estrutura Forum
    
    * :art: Update Models, Template && Views
    
    Update Models, Template && Views
    
    * hotfix/submissao (#28)
    
    * feature/Emblemas (#31)
    
    * criacao do models emblema
    
    * alteracao init
    
    * update
    
    * adicionando campo de emblemasGanhos
    
    * registrando Emblemas no admin
    
    * adicionando model Emblema
    
    * adc regra para conceder emblema ao usuário
    
    * adicionando emblemas no template do usuario
    
    * context de emblema nas views
    
    * deleted some models
    
    ---------
    
    Co-authored-by: Anderson Freitas <108918987+freitasanderson@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: luis.nunes <luis.nunes@mail.uft.edu.br>
    Co-authored-by: Rafael Soares <79319841+RafaelSoares12@users.noreply.github.com>
    Co-authored-by: RafaelSoares12 <soares.lopes@mail.uft.edu.br>
    Co-authored-by: Lucas Moura <42685124+luc4sm0ur4@users.noreply.github.com>
    Co-authored-by: luis.nunes <luisj2felipe09@gmail.com>

--- codeJunior/cadastro/models/Pessoa.py
+++ codeJunior/cadastro/models/Pessoa.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from desafios.models.Emblema import Emblema
+
 OPCAO_SEXO = (
 		(None,'Ainda não verificado'),
 		('M', 'Masculino'),
@@ -15,17 +17,12 @@ class Pessoa(models.Model):
     cpf = models.CharField('CPF', max_length=11, null=False, unique=True)
     email = models.CharField(u'Email principal', max_length=255,null=True, unique=True)
     contato = models.CharField('Fone de contato',max_length=255, null=True, blank=True, help_text='Fone de contato com DDD (63) 98765-4321', default=None)
-
     ativo = models.BooleanField(verbose_name=u'Ativo?',default=True, editable=True)
-
     sexo = models.CharField(u'Sexo', max_length=2, null=True, blank=True, choices=OPCAO_SEXO)
-    
     dataNascimento = models.DateField('Data de Nascimento', null=True, blank=True)
-
-
     dataCadastro = models.DateTimeField('Data de Cadastro', auto_now_add=True, null=True)
     dataUltimaAlteracao = models.DateTimeField('Última alteração', null=True, blank=True, auto_now=True)
-
+    emblemasGanhos = models.ManyToManyField(Emblema, related_name='emblemasDoUsuario')
     class Meta:
         verbose_name = 'Pessoa'
         verbose_name_plural = 'Pessoas'
