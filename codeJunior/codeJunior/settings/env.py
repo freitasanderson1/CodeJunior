2dec1cb4e44d8372737753498a12eaf51611a7fa
Anderson Freitas <freitas.dev@proton.me>
2023-12-04 13:27:20 -0300

    :zap: Finish
    Finish

--- codeJunior/codeJunior/settings/env.py
+++ codeJunior/codeJunior/settings/env.py
@@ -4,7 +4,7 @@ SECRET_KEY = "j&8vqzrt_9-bngny95u2mu*gsfdu8%f*h5tl=c*qit_oa9h0r+"
 
 DEBUG_MODE=True
 
-ALLOWED_HOSTS=['localhost','127.0.0.1','0.0.0.0']
+ALLOWED_HOSTS=['localhost','127.0.0.1','0.0.0.0','codejunior.fly.dev']
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8069']
 
 CORS_ALLOWED_ORIGINS = CSRF_TRUSTED_ORIGINS
